a3242150a09561e77acda885927f8bcdbd9b3de2
f41ardu <f41thr@arcor.de>
2020-03-21 16:47:16 +0100

    add units to print out

--- distance.py
+++ distance.py
@@ -18,11 +18,11 @@ rssi_scanner = rssi.RSSI_Scan(interface)
 frequency = 2462 # Mhz 
 signalLevel = -70 # dbm
 distance = 10 ** ((27.55 - (20 * math.log10(frequency)) + math.fabs(signalLevel))/20)
-print("Static Test: %2.2f" % distance)
+print("Static Test: %2.2f m" % distance)
 ssids = []
     
 ap_info = rssi_scanner.getAPinfo(networks=ssids,sudo=False)
     #print(ap_info)
 for k in ap_info:
-    print("SSID: ", k['ssid'] , " Distance: %2.2f" % 10 ** ((27.55 - (20 * math.log10(frequency)) + math.fabs(k['signal']))/20)) 
+    print("SSID: ", k['ssid'] , " Distance: %2.2f m" % 10 ** ((27.55 - (20 * math.log10(frequency)) + math.fabs(k['signal']))/20)) 
           
