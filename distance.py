736a1979adb8232970a1e2090e0f8ec500736135
f41ardu <f41thr@arcor.de>
2020-03-22 08:16:15 +0100

    add some comments (far from beeing perfect)

--- distance.py
+++ distance.py
@@ -15,14 +15,18 @@ from rssi.rssi import *
 interface  = 'wlan0'
 rssi_scanner = RSSI_Scan(interface)
 
+# static test 
 freq = 2462 # Mhz 
 signalLevel = -70 # dbm
 distance = 10 ** ((27.55 - (20 * math.log10(freq)) + math.fabs(signalLevel))/20)
 print("Static Test: %2.2f m" % distance)
+# list of detectect networks
 ssids = []
-signalStrength = []  
+# not used
+signalStrength = []
+# gather network information 
 ap_info = rssi_scanner.getAPinfo(networks=ssids,sudo=True)
-
+# print information and calculate distance 
 for k in ap_info:
     distance = 10 ** ((27.55 - (20 * math.log10(k['frequency']*1000)) + math.fabs(k['signal']))/20)
     print("SSID: ", k['ssid'] ," MAC: ", k['address'], " Frequency: ", int(k['frequency']*1000) , " Signal Level: ", k['signal']  , " Distance: %2.2f m" % distance) 
